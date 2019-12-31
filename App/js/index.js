732eb7ae166b55ad01cef028e1abdebe0fc27627
hezewei1996 <1219782682@qq.com>
2019-12-31 10:34:24 +0800

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -5,152 +5,34 @@ xui.Class('App', 'xui.Module',{
             var host=this, children=[], append=function(child){children.push(child.get(0));};
             
             append(
-                xui.create("xui.UI.Panel")
-                .setHost(host,"ctl_panel3")
-                .setDock("none")
-                .setLeft("5.833333333333333em")
-                .setTop("0.8333333333333334em")
-                .setWidth("45em")
-                .setHeight("49.166666666666664em")
-                .setCaption("Monthly Sales Summary")
-            );
-            
-            host.ctl_panel3.append(
-                xui.create("xui.UI.Div")
-                .setHost(host,"xui_ui_div113")
-                .setClassName("xui-uibase")
-                .setLeft("0em")
-                .setHeight("3em")
-                .setBottom("18.5em")
-                .setZIndex(1002)
-                );
-            
-            host.ctl_panel3.append(
-                xui.create("xui.UI.FusionChartsXT")
-                .setHost(host,"ctl_fusionchartsxt1")
-                .setDock("fill")
-                .setWidth("44em")
-                .setHeight("27.166666666666668em")
-                .setChartType("Pie2D")
-                .setJSONData({
-                    "chart":{
-                        "caption":"Monthly Sales Summary",
-                        "subcaption":"For the year 2006",
-                        "xaxisname":"Month",
-                        "yaxisname":"Sales",
-                        "logoScale":"50",
-                        "logoURL":"http://www.crossui.com/img/logo.png",
-                        "numberprefix":"$",
-                        "useroundedges":"1",
-                        "bgcolor":"FFFFFF,FFFFFF",
-                        "showborder":"0",
-                        "placevaluesinside":"1",
-                        "enablerotation":"0",
-                        "animation":"0"
+                xui.create("xui.UI.Stacks")
+                .setHost(host,"xui_ui_stacks1")
+                .setItems([
+                    {
+                        "id":"a",
+                        "caption":"page1",
+                        "imageClass":"xui-icon-xui"
                     },
-                    "data":[
-                        {
-                            "label":"January",
-                            "value":"17400"
-                        },
-                        {
-                            "label":"February",
-                            "value":"19800"
-                        },
-                        {
-                            "label":"March",
-                            "value":"21800"
-                        },
-                        {
-                            "label":"April",
-                            "value":"23800"
-                        },
-                        {
-                            "label":"May",
-                            "value":"29600"
-                        },
-                        {
-                            "label":"June",
-                            "value":"27600"
-                        }
-                    ],
-                    "styles":{
-                        "definition":[
-                            {
-                                "name":"CanvasAnim",
-                                "type":"animation",
-                                "param":"_xScale",
-                                "start":"0",
-                                "duration":"1"
-                            }
-                        ],
-                        "application":[
-                            {
-                                "toobject":"Canvas",
-                                "styles":"CanvasAnim"
-                            }
-                        ]
+                    {
+                        "id":"b",
+                        "caption":"page2"
+                    },
+                    {
+                        "id":"c",
+                        "caption":"page3"
+                    },
+                    {
+                        "id":"d",
+                        "caption":"page4",
+                        "closeBtn":true,
+                        "optBtn":"xui-uicmd-opt",
+                        "popBtn":true
                     }
-                })
-                );
-            
-            host.ctl_panel3.append(
-                xui.create("xui.UI.Block")
-                .setHost(host,"ctl_block7")
-                .setDock("bottom")
-                .setHeight("18.583333333333332em")
-                .setOverflow("hidden")
-                );
-            
-            host.ctl_block7.append(
-                xui.create("xui.UI.Panel")
-                .setHost(host,"xui_ui_panel27")
-                .setLeft("25em")
-                .setTop("6.666666666666667em")
-                .setCaption("Data Source")
-                );
-            
-            host.xui_ui_panel27.append(
-                xui.create("xui.UI.TreeGrid")
-                .setHost(host,"xui_ui_treegrid24")
-                .setDirtyMark(false)
+                ])
                 .setLeft("0em")
                 .setTop("0em")
-                .setEditable(true)
-                .setRowHandler(false)
-                .setHeader([
-                    {
-                        "id":"label",
-                        "caption":"Month",
-                        "type":"input",
-                        "flexSize":true,
-                        "width":"8em"
-                    },
-                    {
-                        "id":"value",
-                        "caption":"Sales",
-                        "type":"currency",
-                        "flexSize":true,
-                        "width":"16em"
-                    }
-                ])
-                .setTreeMode("none")
-                .afterCellUpdated([
-                    {
-                        "desc":"Action 1",
-                        "type":"control",
-                        "target":"ctl_fusionchartsxt1",
-                        "args":[
-                            "{page.ctl_fusionchartsxt1.fillData()}",
-                            undefined,
-                            undefined,
-                            "{page.xui_ui_treegrid24.getRawData()}"
-                        ],
-                        "method":"fillData",
-                        "redirection":"other:callback:call"
-                    }
-                ])
-                );
+                .setValue("a")
+            );
             
             return children;
             // ]]Code created by CrossUI RAD Studio
